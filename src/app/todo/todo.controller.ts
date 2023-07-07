09d52126177b5f8ab19c55bc8800574393b87b41
Alexandre K. Teodoreto <alekteodoreto@hotmail.com>
2023-07-07 20:12:10 -0300

    feat: Controller do módulo Todo
    Criando Controller com métodos de Crud do módulo de Todo

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { CreateTodoDTO } from './dto/create-todo.dto';
+import { UpdateTodoDTO } from './dto/update-todo.dto';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  async findAll() {
+    return await this.todoService.findAll();
+  }
+
+  @Post()
+  async create(@Body() data: CreateTodoDTO) {
+    return await this.todoService.create(data);
+  }
+
+  @Get(':id')
+  async show(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.todoService.findOneOrFail(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() data: UpdateTodoDTO,
+  ) {
+    return await this.todoService.update(id, data);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.todoService.deleteById(id);
+  }
+}
